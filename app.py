8965288aec66f6780c0d8c3b10ae65d990555ec2
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-04-26 11:05:53 +0700

    make it better

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, redirect, url_for, render_template, request
 from datetime import datetime
+from script import DB_NAME, DB_USER,DB_HOST,DB_PASS
+import psycopg2.extras
 
 app = Flask(__name__)
+
+conn = psycopg2.connect(dbname = DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+# cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
 @app.route('/')
@@ -25,41 +30,55 @@ def invent():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        user = request.form["nm"]
-        user_weight = request.form["wt"]
+        product_name = request.form["nm"]
+        product_weight = request.form["wt"]
 
-        user_weight = user_weight.replace('+', ' ')
+        product_weight = product_weight.replace('+', ' ')
 
-        def max_numbers(user_weight):
-            return sum([float(i) for i in user_weight.replace(',', '.').split()])
+        def max_numbers(product_weight):
+            return sum([float(i) for i in product_weight.replace(',', '.').split()])
 
-        all_weight = max_numbers(user_weight)
+        all_weight = max_numbers(product_weight)
 
         def all_weight_numbers(all_weight, dec=0):
             prc = "{:." + str(dec) + "f}"  # first cast decimal as str
             # str format output is {:.3f}
             return prc.format(all_weight)
 
-        user_weight = all_weight_numbers(all_weight)
+        product_weight = all_weight_numbers(all_weight)
 
-        # user_and_weight = user, user_weight
+        # user_and_weight = product_name, user_weight
         # for i in user_and_weight():
         #     if i == True:
         #         return user_and_weight()
-        print(user, user_weight)
+        print(product_name, product_weight)
 
         def file_saving_process():
-            name_save = user.lower()
-            weight_save = user_weight
-            filename1 = datetime.now().strftime("%Y%m%d-%H%M%S")
+            with conn:
+                with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                    # product_name = input("Введите название: ")
+                    # user_weight = input("Введите вес: ")
+                    cur.execute("INSERT INTO invent_april (name, weight) VALUES(%s, %s)",
+                                ((product_name.lower()), int(product_weight),))
 
-            f = open(f"/Users/new/PycharmProjects/inventarizaciya_final/templates/files/{filename1}.csv", "w")
+                    cur.execute("SELECT * FROM invent_april;")
+                    print(cur.fetchall())
 
-            f.write(f"\n {name_save}  {weight_save}")
+            conn.close()
 
-            f.close()
 
-        user_and_weight = user, user_weight
+
+            # name_save = product_name.lower()
+            # weight_save = user_weight
+            # filename1 = datetime.now().strftime("%Y%m%d-%H%M%S")
+            #
+            # f = open(f"/Users/new/PycharmProjects/inventarizaciya_final/templates/files/{filename1}.csv", "w")
+            #
+            # f.write(f"\n {name_save}  {weight_save}")
+            #
+            # f.close()
+
+        # user_and_weight = product_name, product_weight
 
         def contact():
             if "save" in request.form:
@@ -67,7 +86,7 @@ def login():
                 print("save ok")
 
                 return render_template("/loging.html")
-                    # redirect(url_for("user", usr = user_and_weight))
+                    # redirect(url_for("product_name", usr = user_and_weight))
             elif "cancel" in request.form:
                 print("cancel saving process")
                 return render_template('loging.html')
@@ -78,14 +97,14 @@ def login():
         return redirect(url_for("login"))
         # return render_template("/login1")
 
-        # return redirect(url_for("user", usr=user_and_weight))
+        # return redirect(url_for("product_name", usr=user_and_weight))
         # return render_template('loging.html')
 
         # return render_template("loging.html")
-        # user_and_weight = user, user_weight
-        # return redirect(url_for("user", usr = user_and_weight))
+        # user_and_weight = product_name, user_weight
+        # return redirect(url_for("product_name", usr = user_and_weight))
 
-        # return redirect(url_for("user", usr = user_and_weight))
+        # return redirect(url_for("product_name", usr = user_and_weight))
                                               # and "user_weight", wght = user_weight))
     else:
         return render_template("loging.html")
---
+++ script.py
@@ -0,0 +1,89 @@
+import psycopg2.extras
+import os
+
+DB_HOST = "ec2-3-218-171-44.compute-1.amazonaws.com"
+DB_NAME = "ddd4mnclae5h30"
+DB_USER = "atwrtaatphuwev"
+DB_PASS = "bbe8f3b93fd8c58cb9ac8c952b4cd9daf74edd1454bec9d83088aaad2d889788"
+
+
+
+# conn = psycopg2.connect(dbname = DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+#
+# with conn:
+#     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+#         name = input("Введите название: ")
+#         user_weight = input("Введите вес: ")
+#
+#         cur.execute("INSERT INTO invent_april (name, weight) VALUES(%s, %s)", ((name.lower()), int(user_weight),))
+#
+#         cur.execute("SELECT * FROM invent_april;")
+#         print(cur.fetchall())
+#
+# conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+# cur.execute("CREATE TABLE invent_april (id SERIAL PRIMARY KEY, name VARCHAR, weight VARCHAR);")
+# cur.execute("DROP TABLE invent_april;")
+# cur.executescript("""
+#     INSERT INTO invent_april (name) VALUES(%s)", ("Бананы",);
+#     INSERT INTO invent_april (weight) VALUES(%s)", ("3000",);
+#     """)
+
+# cur.execute("INSERT INTO invent_april (name, weight) VALUES(%s, %s)”, ("Бананы", “3000”,))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # cur.execute("CREATE TABLE student (id SERIAL PRIMARY KEY, name VARCHAR);")
+
+    # cur.execute("INSERT INTO student (name) VALUES(%s)", ("Sanda",))
+
+    # cur.execute("SELECT * FROM student;")
+
+
+# with conn:
+#     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+#         # cur.execute("SELECT * FROM student WHERE id = %s",(1,))
+#         cur.execute("SELECT * FROM student;")
+#         print(cur.fetchall())
+        # print(cur.fetchone()["name"])
+        # cur.execute("INSERT INTO student (name) VALUES(%s)", ("Brad",))
+
+# conn.commit()
+#
+# cur.close()
+
+# conn.close()
