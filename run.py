d09d2f02f8db765d92da0c3b8c47cb7e613d312e
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-05-25 19:11:26 +0700

    make it better

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from app import app
 import os
-app.secret_key = "super secret key"
+# app.secret_key = "super secret key"
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
+
 if __name__ == "__main__":
-    app.secret_key = "super secret key"
+    # app.secret_key = "super secret key"
     app.run(debug=True)
     app.run()
 
